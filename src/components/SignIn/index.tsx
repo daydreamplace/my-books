5b9f538d85c72ed5c487b8683f4b3a2fdcfc2efa
youbin <you8inpark@gmail.com>
2023-02-08 23:47:54 +0900

    💄style email&password input

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import { Row, Col, Input } from "antd";
 
 const SignInComponents = () => {
   return (
-    <div>
-      <h1>signin</h1>
-    </div>
+    <Row align="middle">
+      <Col span={24}>
+        <Row>
+          <Col span={12}></Col>
+          <Col span={12}>
+            <div>My Books</div>
+            <div>Please Note Your Opinion</div>
+            <div />
+            <div>
+              Email
+              <span> *</span>
+              <div>
+                <Input placeholder="Email" autoComplete="email" name="email" />
+              </div>
+            </div>
+            <div>
+              Password
+              <span> *</span>
+              <div>
+                <Input
+                  type="password"
+                  autoComplete="current-password"
+                  name="password"
+                />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 };
 
--- src/redux/modules/auth.ts
+++ src/redux/modules/auth.ts
@@ -44,3 +44,6 @@ const reducer = handleActions<AuthState, string>(
 );
 
 export default reducer;
+
+//saga
+export function* authSaga() {}
